4ed2433bb452288e2d3d72166c9e2720e527749a
unknown <alirahmanfuad@gmail.com>
2022-09-13 00:03:23 +0700

    commit pertemuan 3

---
+++ Pertemuan 3/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    /*int x = 3000;
+    cout << "heloo i love you" << x;*/
+
+    int a = 27;
+    float b = 168.99;
+    char c = 'L';
+    string size = "XXL";
+    string name = "Fuad Ali Rahman";
+
+    cout << "Halo nama saya " << name;
+    cout << "\ntinggi badan : " << b;
+    cout << "\numur : " << a;
+    cout << "\nukuran baju : " << c;
+    cout << "\nukuran celana : " << size;
+
+    return 0;
+}
