d17d4e4a1b861a660620e2e9b808fb41f437973d
Fuad Ali Rahman <alirahmanfuad@gmail.com>
2022-11-14 16:17:38 +0700

    pertemuan 15

---
+++ UTS/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+    cin >> c;
+    for(c; c > 0; c--){
+       c = a*b;
+       c = a;
+       a = b-1;
+       cout << c <<endl;
+
+    }
+    return 0;
+}
